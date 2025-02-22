c174f4cf1b32bbf96c19f61f2cb56af0c10cb113
ccwwhhh <80567425+ccwwhhh@users.noreply.github.com>
2025-02-22 09:51:00 +0800

    Add files via upload

---
+++ util/structure.py
@@ -0,0 +1,28 @@
+import numpy as np
+import torch
+
+import random
+random.seed(1)
+import numpy as np
+np.random.seed(1)
+
+class PointWiseFeedForward(torch.nn.Module):
+    def __init__(self, hidden_units, dropout_rate,activation='relu'):
+        super(PointWiseFeedForward, self).__init__()
+        act = None
+        if activation == 'relu':
+            act = torch.nn.ReLU()
+        elif activation == 'gelu':
+            act = torch.nn.GELU()
+        self.pwff = torch.nn.Sequential(
+            torch.nn.Linear(hidden_units, hidden_units),
+            #torch.nn.Dropout(p=dropout_rate),
+            act,
+            torch.nn.Linear(hidden_units, hidden_units),
+            torch.nn.Dropout(p=dropout_rate)
+        )
+
+    def forward(self, inputs):
+        outputs = self.pwff(inputs)
+        outputs += inputs
+        return outputs
